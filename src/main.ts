a880e16395cbcedd3e3b7bb48407a44320d18ef8
sripriya4 <72649098+sripriya4@users.noreply.github.com>
2026-04-27 11:13:57 +0530

    updated main.ts by removing dotenv for prod

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
-import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// ✅ Load .env ONLY in local (not in Railway)
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: [
       'https://smart-report-extractor-frontend.vercel.app',
       'http://localhost:5173',
     ],
   });
+
   const port = parseInt(process.env.PORT || '3000', 10);
+
+  console.log('🚀 PORT:', port);
+  console.log('🔑 GROQ KEY:', process.env.GROQ_API_KEY ? 'FOUND' : 'MISSING');
+
   await app.listen(port, '0.0.0.0');
-  console.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
